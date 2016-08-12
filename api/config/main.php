b9afba871ddcbc92be833490f3d8e46087e3f9e0
Fadi Ghattas <fadi@cloudappers.com>
2016-08-12 15:35:04 +0300

    update yii error response for login

--- api/config/main.php
+++ api/config/main.php
@@ -23,13 +23,7 @@ return [
             'class' => 'yii\web\Response',
             'on beforeSend' => function ($event) {
                 $response = $event->sender;
-                if ($response->data !== null && Yii::$app->request->get('suppress_response_code')) {
-                    $response->data = [
-                        'success' => $response->isSuccessful,
-                        'data' => $response->data,
-                    ];
-                    $response->statusCode = 200;
-                } else {
+                 if($response->statusCode != 200) {
                     $response->data = [
                         'status' => 'error',
                         'message' => $response->data['message'],
