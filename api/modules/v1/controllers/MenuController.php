31030ad502712263cd327ec3b839221b47804c70
Fadi Ghattas <fadi@cloudappers.com>
2016-08-19 13:31:31 +0300

    check htaccess

--- api/modules/v1/controllers/MenuController.php
+++ api/modules/v1/controllers/MenuController.php
@@ -76,40 +76,7 @@ class MenuController extends ActiveController
 
     public function actionView()
     {
-        $get_data = Yii::$app->request->get();
-        $headers = Yii::$app->getRequest()->getHeaders();
-        $restaurantManager = User::findIdentityByAccessToken(explode(' ', $headers['authorization'])[1]);
-        $restaurant = Restaurants::find()->where(['user_id' => $restaurantManager->id])->one();
-
-        if(empty($restaurantManager))
-            throw new NotFoundHttpException('User not found');
-        if(User::getRoleName($restaurantManager->id) != User::RESTAURANT_MANAGER)
-            throw new ForbiddenHttpException('This account is not a restaurant account');
-        if(!$restaurant->status)
-            throw new ForbiddenHttpException('This account is deactivated');
-
-        $menuCategoryItems = MenuCategories::find()
-            ->where(['menu_categories.id' => $get_data['id']])
-            ->joinWith(['menuCategoryItems'], true, 'INNER JOIN')
-            ->joinWith(['menuCategoryItems', 'menuCategoryItems.menuItem'], true, 'INNER JOIN')
-            ->asArray()->all();
-
-        if(!is_null($menuCategoryItems[0]['deleted_at']))
-            return Helpers::UnprocessableEntityHttpException("This menu category was deleted and we can't get the menu items", null);
-        if($restaurant->id != intval($menuCategoryItems[0]['restaurant_id']))
-            return Helpers::UnprocessableEntityHttpException('This menu category is not belong to this restaurant',null);
-
-        $menuItems = array();
-        foreach ($menuCategoryItems[0]['menuCategoryItems'] as $menuItem)
-        {
-            if(!empty($menuItem['menuItem'])) {
-                $singleMenuItem = array();
-                $singleMenuItem[$menuItem['menuItem']['id']] = $menuItem['menuItem']['name'];
-                $menuItems[] = $singleMenuItem;
-            }
-        }
-
-        return ['success' => 'true' , 'message' => 'get success', 'data' => $menuItems];
+        return 1;
     }
 
 
