023e7e9c0288118133ad340961a34e71450219e2
Fadi Ghattas <fadi@cloudappers.com>
2016-09-03 08:39:18 +0300

    update database tables

--- console/migrations/m160830_173100_order_item_addon_order_item_choices.php
+++ console/migrations/m160830_173100_order_item_addon_order_item_choices.php
@@ -21,38 +21,38 @@ class m160830_173100_order_item_addon_order_item_choices extends Migration
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
-        if (!$this->tableExists('order_item_addon')) {
-            $this->createTable('order_item_addon', [
-                'order_item_id' => $this->integer(10)->unsigned()->notNull(),
-                'addon_id' => $this->integer(10)->unsigned()->notNull(),
-            ], 'ENGINE = InnoDB');
-        }
-
-        $this->createIndex('IDX_Order_Item_Addon_Order_Item_id', 'order_item_addon', 'order_item_id');
-        $this->addForeignKey('FK_Order_Item_Addon_Order_Item_id', 'order_item_addon', 'order_item_id', 'order_items', 'id');
-
-        $this->createIndex('IDX_Order_Item_Addon_Addon_id', 'order_item_addon', 'addon_id');
-        $this->addForeignKey('FK_Order_Item_Addon_Addon_id', 'order_item_addon', 'addon_id', 'addons', 'id');
-
-        $this->addPrimaryKey('order_item_addon_key', 'order_item_addon', ['order_item_id', 'addon_id']);
-
-        if (!$this->tableExists('order_item_choices')) {
-            $this->createTable('order_item_choices', [
-                'order_item_id' => $this->integer(10)->unsigned()->notNull(),
-                'item_choice_id' => $this->integer(10)->unsigned()->notNull(),
-            ], 'ENGINE = InnoDB');
-        }
-
-        $this->createIndex('IDX_Order_Item_Choices_Order_Item_id', 'order_item_choices', 'order_item_id');
-        $this->addForeignKey('FK_Order_Item_Choices_Order_Item_id', 'order_item_choices', 'order_item_id', 'order_items', 'id');
-
-        $this->createIndex('IDX_Order_Item_Choices_Item_Choice_id', 'order_item_choices', 'item_choice_id');
-        $this->addForeignKey('FK_Order_Item_Choices_Item_Choice_id', 'order_item_choices', 'item_choice_id', 'item_choices', 'id');
-
-        $this->addPrimaryKey('order_item_choices_key', 'order_item_choices', ['order_item_id', 'item_choice_id']);
-
-
-        $this->dropColumn('order_items', 'addon');
+//        if (!$this->tableExists('order_item_addon')) {
+//            $this->createTable('order_item_addon', [
+//                'order_item_id' => $this->integer(10)->unsigned()->notNull(),
+//                'addon_id' => $this->integer(10)->unsigned()->notNull(),
+//            ], 'ENGINE = InnoDB');
+//        }
+//
+//        $this->createIndex('IDX_Order_Item_Addon_Order_Item_id', 'order_item_addon', 'order_item_id');
+//        $this->addForeignKey('FK_Order_Item_Addon_Order_Item_id', 'order_item_addon', 'order_item_id', 'order_items', 'id');
+//
+//        $this->createIndex('IDX_Order_Item_Addon_Addon_id', 'order_item_addon', 'addon_id');
+//        $this->addForeignKey('FK_Order_Item_Addon_Addon_id', 'order_item_addon', 'addon_id', 'addons', 'id');
+//
+//        $this->addPrimaryKey('order_item_addon_key', 'order_item_addon', ['order_item_id', 'addon_id']);
+//
+//        if (!$this->tableExists('order_item_choices')) {
+//            $this->createTable('order_item_choices', [
+//                'order_item_id' => $this->integer(10)->unsigned()->notNull(),
+//                'item_choice_id' => $this->integer(10)->unsigned()->notNull(),
+//            ], 'ENGINE = InnoDB');
+//        }
+//
+//        $this->createIndex('IDX_Order_Item_Choices_Order_Item_id', 'order_item_choices', 'order_item_id');
+//        $this->addForeignKey('FK_Order_Item_Choices_Order_Item_id', 'order_item_choices', 'order_item_id', 'order_items', 'id');
+//
+//        $this->createIndex('IDX_Order_Item_Choices_Item_Choice_id', 'order_item_choices', 'item_choice_id');
+//        $this->addForeignKey('FK_Order_Item_Choices_Item_Choice_id', 'order_item_choices', 'item_choice_id', 'item_choices', 'id');
+//
+//        $this->addPrimaryKey('order_item_choices_key', 'order_item_choices', ['order_item_id', 'item_choice_id']);
+//
+//
+//        $this->dropColumn('order_items', 'addon');
         //$this->truncateTable('item_choices');
         $this->dropForeignKey('order_items_choice_id_foreign', 'order_items');
         $this->dropColumn('order_items', 'choice_id');
