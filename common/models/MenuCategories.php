ab7f4c86d9f1f32ef0fd4af402f4da956f8a252a
Fadi <fadi@cloudappers.com>
2016-11-12 20:49:28 +0200

    update  menu items order on update_date in  getMenuCategoryItemsResponse

--- common/models/MenuCategories.php
+++ common/models/MenuCategories.php
@@ -10,14 +10,14 @@ use common\helpers\Helpers;
 /**
  * This is the model class for table "menu_categories".
  *
- * @property string $id
- * @property string $name
- * @property string $restaurant_id
- * @property string $created_at
- * @property string $updated_at
- * @property string $deleted_at
+ * @property string             $id
+ * @property string             $name
+ * @property string             $restaurant_id
+ * @property string             $created_at
+ * @property string             $updated_at
+ * @property string             $deleted_at
  *
- * @property Restaurants $restaurant
+ * @property Restaurants        $restaurant
  * @property MenuCategoryItem[] $menuCategoryItems
  */
 class MenuCategories extends \yii\db\ActiveRecord
@@ -43,7 +43,7 @@ class MenuCategories extends \yii\db\ActiveRecord
             [['restaurant_id'], 'integer'],
             [['deleted_at'], 'safe'],
             [['name'], 'string', 'max' => 255],
-            [['restaurant_id'], 'exist', 'skipOnError' => true, 'targetClass' => Restaurants::className(), 'targetAttribute' => ['restaurant_id' => 'id']],
+            [['restaurant_id'], 'exist', 'skipOnError' => TRUE, 'targetClass' => Restaurants::className(), 'targetAttribute' => ['restaurant_id' => 'id']],
         ];
     }
 
@@ -88,26 +88,19 @@ class MenuCategories extends \yii\db\ActiveRecord
 
     public static function getMenuCategoryItemsAsArray($restaurant_id, $category_id)
     {
-        return MenuCategories::find()
-            ->where(['menu_categories.restaurant_id' => $restaurant_id])
-            ->andWhere(['menu_categories.id' => $category_id])
-            ->andWhere(['menu_items.deleted_at' => null])
-            ->andWhere(['menu_categories.deleted_at' => null])
-            ->joinWith(['menuCategoryItems'], true, 'INNER JOIN')
-            ->joinWith(['menuCategoryItems', 'menuCategoryItems.menuItem'], true, 'INNER JOIN')
-             ->orderBy('menu_items.updated_at DESC')
-            ->orderBy('menu_items.created_at DESC')
-            ->asArray()->all();
+        return MenuItems::find()->joinWith(['menuCategoryItems'])
+            ->where(['menu_category_item.menu_category_id' => $category_id])
+            ->orderBy('created_at DESC')->orderBy('updated_at DESC')->all();
     }
 
     public static function getCategory($restaurant_id, $category_id)
     {
-        return MenuCategories::find()->where(['restaurant_id' => $restaurant_id])->andWhere(['id' => $category_id])->andWhere(['deleted_at' => null])->one();
+        return MenuCategories::find()->where(['restaurant_id' => $restaurant_id])->andWhere(['id' => $category_id])->andWhere(['deleted_at' => NULL])->one();
     }
 
     public static function getMenuCategoryByName($restaurant_id, $category_name)
     {
-        return self::find()->where(['restaurant_id' => $restaurant_id])->andWhere(['name' => $category_name])->andWhere(['deleted_at' => null])->one();
+        return self::find()->where(['restaurant_id' => $restaurant_id])->andWhere(['name' => $category_name])->andWhere(['deleted_at' => NULL])->one();
     }
 
     public static function isCategoryDeleted($restaurant_id, $category_id)
@@ -118,38 +111,32 @@ class MenuCategories extends \yii\db\ActiveRecord
     public static function getMenuCategories()
     {
         $restaurant = Restaurants::checkRestaurantAccess();
-        return Helpers::formatResponse(true, 'get success', $restaurant->menuCategories);
+        return Helpers::formatResponse(TRUE, 'get success', $restaurant->menuCategories);
     }
 
     public static function getMenuCategoryItemsResponse($category_id)
     {
         $restaurant = Restaurants::checkRestaurantAccess();
 
-        if (self::isCategoryDeleted($restaurant->id, $category_id))
+        $menuCategory = MenuCategories::find()->where(['restaurant_id' => $restaurant->id])->andWhere(['id' => $category_id])->one();
+        if (empty($menuCategory))
             return Helpers::HttpException(404, 'get failed', ['error' => "This category dos't exist"]);
+        if (!empty($menuCategory->deleted_at))
+            return Helpers::HttpException(422, 'validation failed', ['error' => "This menu category was deleted and we can't get the menu items"]);
 
         $menuCategoryItems = self::getMenuCategoryItemsAsArray($restaurant->id, $category_id);
-        if (empty($menuCategoryItems))
-            return Helpers::formatResponse(true, 'get success', $menuCategoryItems);
-        if (!is_null($menuCategoryItems[0]['deleted_at']))
-            return Helpers::HttpException(422, 'validation failed', ['error' => "This menu category was deleted and we can't get the menu items"]);
-        if ($restaurant->id != intval($menuCategoryItems[0]['restaurant_id']))
-            return Helpers::HttpException(422, 'validation failed', ['error' => 'This menu category is not belong to this restaurant']);
-
-        $menuItems = array();
-        foreach ($menuCategoryItems[0]['menuCategoryItems'] as $menuItem) {
-            if (!empty($menuItem['menuItem'])) {
-                $singleMenuItem = array();
-                $singleMenuItem['id'] = (int)$menuItem['menuItem']['id'];
-                $singleMenuItem['name'] = (string)$menuItem['menuItem']['name'];
-                $singleMenuItem['price'] = (double)$menuItem['menuItem']['price'];
-                $singleMenuItem['status'] = (bool)$menuItem['menuItem']['status'];
-                $singleMenuItem['is_verified'] = (bool)$menuItem['menuItem']['is_verified'];
-                $menuItems[] = $singleMenuItem;
-            }
+        $menuItems = [];
+        foreach ($menuCategoryItems as $menuItem) {
+            $singleMenuItem = [];
+            $singleMenuItem['id'] = (int)$menuItem->id;
+            $singleMenuItem['name'] = (string)$menuItem->name;
+            $singleMenuItem['price'] = (double)$menuItem->price;
+            $singleMenuItem['status'] = (bool)$menuItem->status;
+            $singleMenuItem['is_verified'] = (bool)$menuItem->is_verified;
+            $menuItems[] = $singleMenuItem;
         }
 
-        return Helpers::formatResponse(true, 'get success', $menuItems);
+        return Helpers::formatResponse(TRUE, 'get success', $menuItems);
     }
 
     public static function createCategory($data)
@@ -167,8 +154,8 @@ class MenuCategories extends \yii\db\ActiveRecord
 
         $isCreated = $menCategory->save();
         if (!$isCreated)
-            return Helpers::HttpException(422, 'create failed', null);
-        return Helpers::formatResponse(true, 'create success', ['id' => $menCategory->id]);
+            return Helpers::HttpException(422, 'create failed', NULL);
+        return Helpers::formatResponse(TRUE, 'create success', ['id' => $menCategory->id]);
     }
 
     public static function updateCategory($category_id, $data)
@@ -191,9 +178,9 @@ class MenuCategories extends \yii\db\ActiveRecord
 
         $isUpdated = $menCategory->save();
         if (!$isUpdated)
-            return Helpers::HttpException(422, 'update failed', null);
+            return Helpers::HttpException(422, 'update failed', NULL);
 
-        return Helpers::formatResponse(true, 'update success', ['id' => $menCategory->id]);
+        return Helpers::formatResponse(TRUE, 'update success', ['id' => $menCategory->id]);
     }
 
     public static function deleteCategory($category_id)
@@ -212,9 +199,9 @@ class MenuCategories extends \yii\db\ActiveRecord
         $isUpdated = $menCategory->save();
 
         if (!$isUpdated)
-            return Helpers::HttpException(422, 'deleted failed', null);
+            return Helpers::HttpException(422, 'deleted failed', NULL);
 
-        return Helpers::formatResponse(true, 'deleted success', ['id' => $menCategory->id]);
+        return Helpers::formatResponse(TRUE, 'deleted success', ['id' => $menCategory->id]);
     }
 
     public function afterValidate()
@@ -245,7 +232,7 @@ class MenuCategories extends \yii\db\ActiveRecord
                     },
                     'name' => function () {
                         return (string)$this->name;
-                    }
+                    },
                 ],
                 self::SCENARIO_GET_DETAILS_BY_CLIENT => [
                     'id' => function () {
@@ -258,8 +245,8 @@ class MenuCategories extends \yii\db\ActiveRecord
                         $menuCategories = self::getMenuCategoryItemsAsArray($this->restaurant_id, $this->id);
                         if (!empty($menuCategories[0]['menuCategoryItems']))
                             return MenuItems::formatMenuCategoryItems($menuCategories[0]['menuCategoryItems'], $this->restaurant->is_verified_global);
-                        else return array();
-                    }
+                        else return [];
+                    },
                 ],
             ]);
     }
